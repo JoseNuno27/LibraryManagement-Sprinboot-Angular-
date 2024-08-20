b500e9fa9f44cb1469db7723481278146abbc4fa
Diogo Ferreira <a48530@alunos.ipb.pt>
2024-08-20 18:10:26 +0100

    update

--- com.inn.library/src/main/java/com/inn/library/constents/LibraryConstant.java
+++ com.inn.library/src/main/java/com/inn/library/constents/LibraryConstant.java
@@ -3,4 +3,6 @@ package com.inn.library.constents;
 public class LibraryConstant {
     public static final String SOMETHING_WENT_WRONG = "Aconteceu um erro";
 
+    public static final String INVALID_DATA= "Invalid Data.";
+
 }
--- com.inn.library/src/main/java/com/inn/library/servicelmpl/UserServiceImpl.java
+++ com.inn.library/src/main/java/com/inn/library/servicelmpl/UserServiceImpl.java
@@ -1,24 +1,67 @@
 package com.inn.library.servicelmpl;
 
+import com.inn.library.POJO.User;
+import com.inn.library.constents.LibraryConstant;
+import com.inn.library.dao.UserDao;
 import com.inn.library.service.UserService;
+import com.inn.library.utlis.LibraryUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.Objects;
 
 @Service
 @Slf4j
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    UserDao userDao;
 
     @Override
     public ResponseEntity<String> signUp(Map<String, String> requestMap) {
+        log.info("Inside signup {}", requestMap);
 
-        return null;
+        try {
+            if (validateSignUp(requestMap)) {
+                User user = userDao.findByEmailId(requestMap.get("email"));
+                if (Objects.isNull(user)) {
+                    userDao.save(getUserFromMap(requestMap));
+                    return LibraryUtils.getResponseEntity("Successfully Registered.", HttpStatus.OK);
+
+                } else {
+                    return LibraryUtils.getResponseEntity("Email already exits ", HttpStatus.BAD_REQUEST);
+                }
+
+            } else {
+                return LibraryUtils.getResponseEntity(LibraryConstant.INVALID_DATA, HttpStatus.BAD_REQUEST);
+            }
+        }catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        return LibraryUtils.getResponseEntity(LibraryConstant.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     private boolean validateSignUp(Map<String, String> requestMap) {
-        return requestMap.containsKey("name") && requestMap.containsKey("contactNumber") && requestMap.containsKey("password") && requestMap.containsKey("email");
+        if (requestMap.containsKey("name") && requestMap.containsKey("contactNumber") && requestMap.containsKey("password") && requestMap.containsKey("email")) {
+            return true;
+        }
+
+        return false;
+
+    }
+    private User getUserFromMap(Map<String,String> requestMap){
+        User user = new User();
+        user.setName(requestMap.get("name"));
+        user.setContactNumber(requestMap.get("contactNumber"));
+        user.setEmail(requestMap.get("email"));
+        user.setPassword(requestMap.get("password"));
+        user.setStatus("false");
+        user.setRole("user");
+        return user;
     }
 }
